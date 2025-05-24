0ad5fbbfa81dbf2574900abf003ec1d5c1186e4c
Shardul Aswale <53704230+ShardulAswale@users.noreply.github.com>
2025-05-24 10:29:36 +0100

    Feat: version 6 added test for sum_value()

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,14 @@
+#Test suite for sample.py functions.
+
 import sample
 
-def test_hello():
+def test_hello() -> None:
     assert sample.hello() == "Hello, world!"
 
-def test_greet():
+
+def test_greet() -> None:
     assert sample.greet("Alice") == "Hello, Alice!"
+
+
+def test_sum_values() -> None:
+    assert sample.sum_values(4, 5) == "Sum is 9"
